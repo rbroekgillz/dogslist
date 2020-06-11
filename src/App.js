f9724e7ec564e6884726499daa0f6a5ebdd1ef33
rbroekgillz <r.broek@gillz.nl>
2020-06-11 11:08:48 +0200

    Add DogBreedImages component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import DogsListContainer from "./components/DogsListContainer";
+import DogBreedImages from "./components/DogBreedImages";
+
+import {Route} from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
             <main>
-                <DogsListContainer/>
+                <Route exact path="/" component={DogsListContainer}/>
+                <Route path={"/dog-breeds/:breed"} component={DogBreedImages}/>
             </main>
         </div>
     );
