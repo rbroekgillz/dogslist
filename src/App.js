bce97900e37eb31b5980fb20d851d452b8d7dc2c
rbroekgillz <r.broek@gillz.nl>
2020-06-11 11:19:15 +0200

    Add option to view dog images

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import DogsListContainer from "./components/DogsListContainer";
 import DogBreedImages from "./components/DogBreedImages";
 
 import {Route} from 'react-router-dom';
+import DogBreedImagesContainer from "./components/DogBreedImagesContainer";
 
 function App() {
     return (
         <div className="App">
             <main>
                 <Route exact path="/" component={DogsListContainer}/>
-                <Route path={"/dog-breeds/:breed"} component={DogBreedImages}/>
+                <Route path={"/dog-breeds/:breed"} component={DogBreedImagesContainer}/>
             </main>
         </div>
     );
